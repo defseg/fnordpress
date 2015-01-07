5a59024c2b4baabfa071a62d2746cdf7ba6e9f8b
defseg <6988161+defseg@users.noreply.github.com>
2015-01-07 11:40:28 -0500

    Add login and signup

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if user
+      log_in(user)
+      redirect_to new_session_url
+    else
+      flash.now[:errors] = user.errors.full_messages
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to new_session_url
+  end
+end
